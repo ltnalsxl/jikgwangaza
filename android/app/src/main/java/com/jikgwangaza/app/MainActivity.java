89e778fc1753fe1a89348734d48aaf497af46d98
Sumin Lee <fltnadls1011@gmail.com>
2025-07-10 01:21:41 +0900

    ios develop

---
+++ android/app/src/main/java/com/jikgwangaza/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jikgwangaza.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jikgwangaza.app',
+  appName: '직관가자',
+  webDir: 'build'
+};
+
+export default config;
