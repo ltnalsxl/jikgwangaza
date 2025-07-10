4a2f84516f16e8ca94d4d05bfc94e56a0fbb6e1f
Sumin Lee <fltnadls1011@gmail.com>
2025-07-10 21:46:59 +0900

    Update app display name to Korean

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="text-center text-xs text-gray-500 dark:text-gray-400 py-6">
-    © 2025 Jikgwangaza. All rights reserved.
+    © 2025 직관가자. All rights reserved.
     <br />
     Created and maintained by Sumin Lee.
     <br />
