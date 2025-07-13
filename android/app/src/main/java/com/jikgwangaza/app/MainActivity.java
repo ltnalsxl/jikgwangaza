bf598bf385f86b2f45d629651090e02d568c2870
Sumin Lee <fltnadls1011@gmail.com>
2025-07-13 15:58:19 +0900

    Remove mobile platforms

--- android/app/src/main/java/com/jikgwangaza/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.jikgwangaza.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.jikgwangaza.app',
-  appName: '직관가자',
-  webDir: 'build'
-};
-
-export default config;
